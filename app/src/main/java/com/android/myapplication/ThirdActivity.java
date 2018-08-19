62b69a072460a06011e7b7880449edb0b72ddade
rshaik <rshaik1211@gmail.com>
2018-08-18 21:16:58 -0400

    Third Branch Created

---
+++ app/src/main/java/com/android/myapplication/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.android.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
